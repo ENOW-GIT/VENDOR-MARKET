f9db316b53b1e74ee551ff924d7be56222164d80
ENOW-GIT <enowelias50@gmail.com>
2024-08-13 14:43:27 +0100

    commit all

---
+++ color.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn")
+const color =document.querySelector(".color");
+
+btn.addEventListener = ("click", function(){
+    const randomIndex = getRandomIndex();
+    console.log(randomIndex);
+    
+    document.body.style.backgroundColor = colors[randomIndex];
+    color.textContent=colors[randomIndex];
+});
+function getRandomIndex(){
+    return Math.floor(Math.random()*colors.length);
+}
